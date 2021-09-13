d472c0cf66067c7e37c52c8681951287ed542ed9
Glitch (reliable-secretive-asteroid) <none>
2021-09-13 05:54:10 +0000

    🎣👳 Updated with Glitch

--- app.js
+++ app.js
@@ -18,3 +18,57 @@ const app = new App({
 
   console.log('⚡️ Bolt app is running!');
 })();
+
+app.event('app_home_opened', async ({ event, client, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await client.views.publish({
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to the Slack Random Chooser !!! * :tada: \n" +
+              "This app is still in development, but at term, the goal is to do the same thing as the random chooser application inside slack."
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now. WIP" //  butt you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Start the scrum!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
